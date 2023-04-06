40164da53534ba4730b4bea6e43302bc5af069c3
adrian6922 <vargaszambranoj@gmail.com>
2023-04-06 10:57:54 -0400

    subida de api bootcamp backEnd

---
+++ bootCampApi/bootcamp-api/app/Http/Controllers/AlumnoController.php
@@ -0,0 +1,101 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Alumno;
+use Illuminate\Http\Request;
+
+class AlumnoController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $alumno = Alumno::where('deleted_at',null)->get();
+        return response()->json( $alumno );
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $alumno = new Alumno();
+        $alumno->nombre = $request->nombre;
+        $alumno->apellido = $request->apellido;
+        $alumno->correo = $request->correo;
+        $alumno->created_at = now();
+        if($alumno->save()):
+            return response()->json([
+                'mensaje' => 'Alumno creado correctamente',
+                'estado' => 200
+            ]);
+        else:
+            return response()->json([
+                'mensaje' => 'Error al crear Alumno',
+                'estado' => 500
+            ]);
+        endif;
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        
+    }
+}
---
+++ bootCampApi/bootcamp-api/app/Models/Alumno.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Alumno extends Model
+{
+    use HasFactory;
+
+    protected $table = 'alumnos';
+    protected $primaryKey = 'idAlumno';
+    protected $fillables = [
+        'nombre',
+        'apellido',
+        'correo'
+    ];
+
+}
